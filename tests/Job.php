d21ccdedbe941a94d2b22f5856d8c57cf43e6537
Radosław Adamowicz <r.adamowicz4@wp.pl>
2019-12-01 00:49:20 +0100

    Dodanie testów

---
+++ tests/Job.php
@@ -0,0 +1,28 @@
+<?php
+
+use Pool\Job\JobException;
+
+class Job extends \Pool\Job\AbstractJob
+{
+    /**
+     * @return void
+     * @throws JobException
+     */
+    public function make(): void
+    {
+        $nOfEle = (int)$this->getData();
+
+        if (4 === $nOfEle) {
+            throw new JobException('An error occurred...');
+        }
+
+        $nOfEle = 10 ** $nOfEle;
+        $text = "job {$this->getJobId()} with PID {$this->getPID()} and parent PID {$this->getParentPID()} ";
+        echo $text . 'started' . PHP_EOL;
+        for ($i = 0; $i < $nOfEle; $i++) {
+            md5(uniqid('', true));
+        }
+        echo $text . 'ended' . PHP_EOL;
+        sleep(10);
+    }
+}
--- tests/test.php
+++ tests/test.php
@@ -1,4 +1,12 @@
 <?php
 require __DIR__ . '/../../../autoload.php';
-// todo2
+require 'Job.php';
 
+$pool = new \Pool\Pool(2);
+$pool->setJobs(array_fill(0, 4, Job::class), [4, 5, 6, 3]);
+
+try {
+    $pool->run();
+} catch (\Pool\Job\JobException $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
