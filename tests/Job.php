837c71e693066afc2a9862fc07f3d16f6f3856ed
Radosław Adamowicz <r.adamowicz4@wp.pl>
2019-12-01 18:08:23 +0100

    Testy

--- tests/Job.php
+++ tests/Job.php
@@ -5,6 +5,29 @@ use Pool\Job\JobException;
 class Job extends \Pool\Job\AbstractJob
 {
     /**
+     * @var string
+     */
+    private $param1;
+
+    /**
+     * @var string
+     */
+    private $param2;
+
+    /**
+     * Job constructor.
+     *
+     * @param string $param1
+     * @param string $param2
+     */
+    public function __construct(string $param1, string $param2)
+    {
+        $this->param1 = $param1;
+        $this->param2 = $param2;
+        echo "{$this->getPID()} param1: $param1; param2: $param2" . PHP_EOL;
+    }
+
+    /**
      * @return void
      * @throws JobException
      */
@@ -13,15 +36,19 @@ class Job extends \Pool\Job\AbstractJob
         $nOfEle = (int)$this->getData();
 
         if (4 === $nOfEle) {
-            throw new JobException('An error occurred...');
+            throw new JobException('An error occurred test...');
         }
 
+        $this->setName($this->getName() . ' ' . $this->param1 . ' ' . $this->param2);
+
         $nOfEle = 10 ** $nOfEle;
-        $text = "job {$this->getJobId()} with PID {$this->getPID()} and parent PID {$this->getParentPID()} ";
+        $text = "job {$this->getJobId()} {$this->getName()} with PID {$this->getPID()} and parent PID {$this->getParentPID()} ";
         echo $text . 'started' . PHP_EOL;
         for ($i = 0; $i < $nOfEle; $i++) {
             md5(uniqid('', true));
         }
+        sleep(5);
         echo $text . 'ended' . PHP_EOL;
+        echo memory_get_peak_usage() / 1024 / 1024 . PHP_EOL;
     }
 }
--- tests/test.php
+++ tests/test.php
@@ -2,11 +2,13 @@
 require __DIR__ . '/../../../autoload.php';
 require 'Job.php';
 
-$pool = new \Pool\Pool(2);
-$pool->setJobs(array_fill(0, 4, Job::class), [4, 5, 6, 3]);
-
 try {
+    $pool = new \Pool\Pool(2);
+    foreach ([4, 5, 6, 3] as $value) {
+        $pool->setJob(new \Pool\Job\JobConfig(Job::class, $value, [md5($value), md5($value ** 10)]));
+    }
+    $pool->setName('master');
     $pool->run();
-} catch (\Pool\Job\JobException $e) {
+} catch (\Pool\Job\JobException|Exception $e) {
     echo $e->getMessage() . PHP_EOL;
 }
